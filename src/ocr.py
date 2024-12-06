6ac7d3b46d8a63570fe519a358486ddc593f0aaf
K-RyuGa <y24m002@mail.ryukoku.ac.jp>
2024-12-06 22:30:59 +0900

    test

--- src/ocr.py
+++ src/ocr.py
@@ -2,7 +2,7 @@ import easyocr
 import cv2
 # from google.colab.patches import cv2_imshow
 reader = easyocr.Reader(['ja','en']) # 文字の選択一回だけでいい
-path = "C:/Users/matsuhira/AppData/Local/Programs/Python/Python312/Hobby/git-kun/src/text/kinkakuji.png"
-
+path = r"C:\Users\salmi\Downloads\test.png"
+#path = r"C:\Users\salmi\git-kun\src\text\kinkakuji.png"
 result2 = reader.readtext(path, detail=0) # 文章のみ
 print(result2)
