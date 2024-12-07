ead4c3cee3c243fb607e23588b2e2b401be04c6b
KenMatsuhira <tz23kmatsuhira@ec.usp.ac.jp>
2024-12-07 09:40:24 +0900

    masanobu

--- src/ocr.py
+++ src/ocr.py
@@ -1,8 +1,27 @@
 import easyocr
-import cv2
-# from google.colab.patches import cv2_imshow
-reader = easyocr.Reader(['ja','en']) # 文字の選択一回だけでいい
-path = "C:/Users/matsuhira/AppData/Local/Programs/Python/Python312/Hobby/git-kun/src/text/kinkakuji.png"
+import os
+import tkinter as tk
+from tkinter import filedialog
 
-result2 = reader.readtext(path, detail=0) # 文章のみ
-print(result2)
+root = tk.Tk()
+root.withdraw()
+path = filedialog.askopenfilename(title="ファイルを選択してください")
+
+reader = easyocr.Reader(['ja','en'])
+# path = "text/tegaki.jpg" # git-kun/src/text/
+combined_text = ""
+
+result = reader.readtext(path, detail=0) # 文章のみ
+result_all = reader.readtext(path, paragraph=True)
+
+for text in result:
+    print(text, end="")
+    combined_text += text
+    combined_text = combined_text.replace('\n', '')
+
+output_dir = 'output_text'
+os.makedirs(output_dir, exist_ok=True)
+
+output_path = os.path.join(output_dir, 'result.txt')
+with open(output_path, 'w', encoding='utf-8') as f:
+    f.write(combined_text)
